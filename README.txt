0392b0e75887cba5bbbd9861204b808561173918
G. Austin Simon <austin@austinsimon.com>
2022-01-19 16:22:26 -0500

    Added README and position logging

---
+++ README.txt
@@ -0,0 +1,5 @@
+HCIRA Project 1
+
+To run, open the executable located in the dist folder.
+
+To clear the canvas, hit the TAB key on the keyboard.
--- main.py
+++ main.py
@@ -3,8 +3,15 @@ app = Tk()
 app.title("Project 1 Canvas")
 app.geometry("400x400")
 
+x_pos = []
+y_pos = []
+
 def clear(event):
     canvas.delete('all')
+    # Clear path list
+    x_pos.clear()
+    y_pos.clear()
+    print(x_pos)
 
 def get_x_and_y(event):
     global curx, cury
@@ -14,6 +21,10 @@ def draw(event):
     global curx, cury
     canvas.create_line((curx, cury, event.x, event.y), fill='red', width=2)
     curx, cury = event.x, event.y
+    # Store location of path
+    x_pos.append(curx)
+    y_pos.append(cury)
+    print(x_pos)
 
 canvas = Canvas(app, bg='black')
 canvas.pack(anchor='nw', fill='both', expand=1)
